efeab0e0f6fe422b0b0af0a3dd67c24324da9ad2
shuuma9981@gmail.com <shuuma9981@gmail.com>
2024-11-21 20:47:53 +0900

    提出18

--- testPaper/middle/question6.jsx
+++ testPaper/middle/question6.jsx
@@ -1,25 +1,37 @@
 import React, { useState } from "react";
 
 const Question6 = ({ userName }) => {
-  const [isChecked, setIsChecked] = useState(false);
+  // Question6というコンポーネントを作成。userNameを受け取る
 
-  // チェックボックスのクリックイベント
-  const handleCheckboxChange = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  // チェックされているかどうかの状態を管理
+
+  const handleCheckboxClick = () => {
+    // チェックボックスがクリックされたときの処理
     setIsChecked(!isChecked);
+    // 状態を反転させる
   };
 
   return (
+    // コンポーネントの表示内容
     <li>
+      {/* リストの1つの項目 */}
       <span style={{ textDecoration: isChecked ? "line-through" : "none" }}>
+        {/* チェックされていたら取り消し線を表示 */}
         {userName}
+        {/* ユーザー名を表示 */}
       </span>
       <input
         type="checkbox"
-        value={isChecked}
-        onChange={handleCheckboxChange}
+        // チェックボックス
+        checked={isChecked}
+        // 状態に応じてチェック状態を設定
+        onClick={handleCheckboxClick}
+        // クリック時に処理を実行
       />
     </li>
   );
 };
 
 export default Question6;
+// このコンポーネントを他のファイルで使えるようにする
--- testPaper/middle/question7.jsx
+++ testPaper/middle/question7.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import Question6 from "./Question6"; // 問題6のコンポーネントをインポート
+import Question6 from "./Question6";
+// Question6コンポーネントをインポート
 
 const Question7 = ({ userNames }) => {
+  // Question7というコンポーネントを作成。userNamesを受け取る
+
   return (
     <ul>
       {userNames
-        .filter((userName) => userName !== "自分") // "自分"は表示しない
+        .filter((userName) => userName !== "自分")
+        // "自分"という名前を除外
         .map((userName, index) => (
-          <Question6 key={index} userName={userName} /> // 問題6のコンポーネントを使用
+          <Question6 key={index} userName={userName} />
+          // 各名前に対してQuestion6コンポーネントを作成
         ))}
     </ul>
   );
 };
 
 export default Question7;
+// このコンポーネントを他のファイルで使えるようにする
